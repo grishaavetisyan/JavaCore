6d30c808bd3855bffa96fe9233ddee11f86a7b4c
Hripsime <hripsimem.yan@gmail.com>
2022-12-24 20:55:01 +0400

    add fix for FigurePainter

--- src/homework/FigurePainter.java
+++ src/homework/FigurePainter.java
@@ -11,6 +11,19 @@ public class FigurePainter {
 
         System.out.println();
 
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            
+        }
+
+        System.out.println();
+
         for (int i = 0; i <= 5; i++) {
             for (int j = 5; j > i; j--) {
                 System.out.print("* ");
