6301c0f20774ac6c7071cbf79812b25d4ec90f19
Hripsime <hripsimem.yan@gmail.com>
2022-12-28 10:21:49 +0400

    add chapter 4's examples 12/28

---
+++ src/chapter4/Ternary.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.print("Bacardzak arjeq ");
+        System.out.println(i + " = " + k);
+
+        i = -10;
+        k = i < 0 ? - i : i;
+        System.out.print("Bacardzak arjeq ");
+        System.out.println(i + " = " + k);
+    }
+}
