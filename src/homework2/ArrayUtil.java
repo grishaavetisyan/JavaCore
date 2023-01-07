2de88d573d04ab9688cd81b20eb621cd7dedc6d5
Hripsime <hripsimem.yan@gmail.com>
2023-01-08 02:16:22 +0400

    add homeworks 1/7

--- src/homework2/ArrayUtil.java
+++ src/homework2/ArrayUtil.java
@@ -27,14 +27,52 @@ public class ArrayUtil {
 
         int maxValue = array[0];
 
-        for (int i = 1; i < array.length ; i++) {
+        for (int i = 1; i < array.length; i++) {
             if (array[i] > maxValue) {
                 maxValue = array[i];
             }
         }
         System.out.println("max value:" + maxValue);
 
+//
+        System.out.print("Մասիվի զույգ էլեմենտներն են՝ ");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+
+        }
+
+        System.out.println();
+
+        System.out.print("Մասիվի կենտ էլեմենտներն են՝ ");
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] % 2 != 0) {
+                System.out.print(array[j] + " ");
+            }
+        }
+        System.out.println();
+//        Տպեք զույգերի քանակը։
+//        Տպեք կենտերի քանակը
+        System.out.print("Մասիվի բոլոր թվերի միջին թվաբանականն է՝ ");
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+            result = result + array[i];
+        }
+        System.out.print(result / 5);
+
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+            result = result + array[i];
+        }
+        System.out.println("Մասիվի էլեմենտների գումարն է՝ " + result);
+        System.out.println("Մասիվի առաջին էլեմենտն է՝ " + array[0]);
+
+        int lastIndex = array.length - 1;
+        System.out.println("Մասիվի վերջին էլեմենտն է՝ " + array[lastIndex]);
+//        Տպեք մասիվի մեջտեղի էլեմենտը։
 
 
+        }
     }
-}
