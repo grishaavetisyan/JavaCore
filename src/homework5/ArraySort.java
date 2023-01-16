e021367442fc73019eae8a9e30f5fbd2f8d71c49
Hripsime <hripsimem.yan@gmail.com>
2023-01-16 22:12:20 +0400

    add changed homeworks 1/16

--- src/homework5/ArraySort.java
+++ src/homework5/ArraySort.java
@@ -5,10 +5,17 @@ import java.util.Arrays;
 public class ArraySort {
     public static void main(String[] args) {
         int[] numbers = {43, 55, 5, -9, 0, 12, 5, 65};
-        Arrays.sort(numbers);
-        for (int x : numbers) {
-            System.out.print(x + ", ");
-
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 1; j < numbers.length - i; j++) {
+                if (numbers[j] < numbers[j-1]) {
+                    int tmp = numbers[j];
+                    numbers[j] = numbers[j - 1];
+                    numbers[j - 1] = tmp;
+                }
+            }
+        }
+        for (int number : numbers) {
+            System.out.print(number + " ");
         }
     }
 }
--- src/homework5/ArraySpaceExample.java
+++ src/homework5/ArraySpaceExample.java
@@ -3,13 +3,31 @@ package homework5;
 public class ArraySpaceExample {
     public static void main(String[] args) {
         char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
-        char first = spaceArray[0];
-        char last = spaceArray[spaceArray.length-1];
+        int first = spaceArray[0];
+        int last = spaceArray[spaceArray.length - 1];
 
-        for (char x : spaceArray) {
-            if ( x != first);
-            if (x != last);
+        for (int i = 0; i < spaceArray.length; i++) {
+            if (spaceArray[i] == ' ') {
+                first++;
+            } else {
+                break;
+            }
         }
-        System.out.print(spaceArray);
+        for (int i = last; i >= 0; i--) {
+            if (spaceArray[i] == ' ') {
+                last--;
+            } else {
+                break;
+            }
+        }
+        char[] result = new char [(last-first)+1];
+        int j = 0;
+        for (int i = first; i <= last; i++) {
+            result[j++] = spaceArray[i];
+        }
+        for (char c : result) {
+            System.out.print(c);
+
+        }
     }
 }
