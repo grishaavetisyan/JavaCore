29e5a012db488af5caabda0feae92a0554c86363
Hripsime <hripsimem.yan@gmail.com>
2023-01-18 01:19:17 +0400

    add 7th homework 1/17

--- src/homework7/CalculatorDemo.java
+++ src/homework7/CalculatorDemo.java
@@ -3,12 +3,9 @@ package homework7;
 public class CalculatorDemo {
     public static void main(String[] args) {
         Calculator myCal = new Calculator();
-        System.out.print("Գումարը հավասար է " + myCal.plus());
-        System.out.println();
-        System.out.print("Տարբերությունը հավասար է " + myCal.minus());
-        System.out.println();
-        System.out.print("Քանորդը հավասար է " + myCal.divide());
-        System.out.println();
-        System.out.print("Արտադրյալը հավասար է " + myCal.multiply());
+        System.out.println("Գումարը հավասար է " + myCal.plus());
+        System.out.println("Տարբերությունը հավասար է " + myCal.minus());
+        System.out.println("Քանորդը հավասար է " + myCal.divide());
+        System.out.println("Արտադրյալը հավասար է " + myCal.multiply());
     }
 }
